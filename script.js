1c8aa339013a1beaa1d8dfb2860a4841c2869cad
XixWISExiX <addalooptoit@gmail.com>
2023-05-18 22:31:11 -0500

    Skeleton of files added

---
+++ script.js
@@ -0,0 +1,3 @@
+// TODO make a 16x16 grid of divs in javascript and put them inside the grid container
+// TODO add a popup asking for the number of squares per side for the new grid (default size is 16x16) also set the limit for the user input to be 100
+// TODO add RGB color
