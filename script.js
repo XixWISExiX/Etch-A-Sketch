49f842b043522e8f94233ba164735860d909c844
XixWISExiX <addalooptoit@gmail.com>
2023-05-19 13:47:12 -0500

    Scalability Feature Added to Grid

--- script.js
+++ script.js
@@ -4,25 +4,63 @@
 import { body } from "./DOMref.js";
 
 function application() {
-  grid();
+  const slider = document.getElementById("slider");
+  const sliderValue = document.getElementById("sliderValue");
+  slider.addEventListener("input", () => {
+    sliderValue.textContent = slider.value;
+    console.log("Value: " + slider.value);
+    clearGrid(slider.value);
+    gridConstruction(slider.value);
+    drawOnBlock();
+  });
+  gridConstruction(slider.value);
   drawOnBlock();
 }
 
-function grid() {
-  const gridContainer = body.getElementsByClassName("gridContainer");
+function clearGrid(blockWidth) {
+  //   body.remove("gridContainer");
+  //   const grid = body.getElementsByClassName("grid");
+  //   const gridContainer = grid[0].getElementsByClassName("gridContainer");
+  //   grid[0].removeChild(gridContainer[0]);
+  const gridElements = document.querySelectorAll(".gridContainer");
+  gridElements.forEach((element) => element.parentNode.removeChild(element));
+}
 
-  for (let i = 0; i < 16; i++) {
+function gridConstruction(blockWidth) {
+  //   const gridContainer = body.getElementsByClassName("gridContainer");
+  const grid = body.getElementsByClassName("grid");
+  const gridContainer = document.createElement("div");
+  gridContainer.classList.add("gridContainer");
+  grid[0].appendChild(gridContainer);
+
+  let borderSize = 1 / blockWidth;
+  let dimentions = 650 / blockWidth - borderSize;
+  for (let i = 0; i < blockWidth; i++) {
     const rowGridContainer = document.createElement("div");
     rowGridContainer.classList.add("rowGridContainer");
-    gridContainer[0].appendChild(rowGridContainer);
-    for (let j = 0; j < 16; j++) {
+    // rowGridContainer.style.borderRight = `solid black 1px`;
+    gridContainer.appendChild(rowGridContainer);
+    for (let j = 0; j < blockWidth; j++) {
       const newDiv = document.createElement("div");
       newDiv.classList.add("gridBlock");
+      newDiv.style.width = `${dimentions}px`;
+      newDiv.style.height = `${dimentions}px`;
+      newDiv.style.borderTop = `solid black 1px`;
+      newDiv.style.borderLeft = `solid black 1px`;
       rowGridContainer.appendChild(newDiv);
     }
   }
-  const gridWidth = 16 * 16 + 16;
-  gridContainer[0].style.width = `${gridWidth}px`;
+  let gridDimentions = dimentions * blockWidth + 20;
+  console.log(gridDimentions);
+  gridContainer.style.width = gridDimentions;
+  gridContainer.style.height = gridDimentions;
+  gridContainer.style.borderBottom = "solid black 1px";
+  gridContainer.style.borderRight = "solid black 1px";
+
+  const gridWidth = blockWidth * blockWidth + parseInt(blockWidth);
+  console.log(dimentions);
+  //   gridContainer.style.width = `${gridWidth}px`;
+  //   gridContainer.style.height = `${gridWidth}px`;
 }
 
 function drawOnBlock() {
