f237ebf39cfd965af8a8414ac7dcbed831750e63
XixWISExiX <addalooptoit@gmail.com>
2023-05-19 15:47:50 -0500

    Clear button added

--- DOMref.js
+++ DOMref.js
@@ -1,1 +1,4 @@
 export const body = document.body;
+export const slider = document.getElementById("slider");
+export const sliderValue = document.getElementById("sliderValue");
+export const clear = document.getElementsByClassName("clear");
--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-// TODO add a popup asking for the number of squares per side for the new grid (default size is 16x16) also set the limit for the user input to be 100
 // TODO add RGB color
 
-import { body } from "./DOMref.js";
+import { body, slider, sliderValue, clear } from "./DOMref.js";
 
 function application() {
-  const slider = document.getElementById("slider");
-  const sliderValue = document.getElementById("sliderValue");
   slider.addEventListener("input", () => {
-    sliderValue.textContent = slider.value;
-    console.log("Value: " + slider.value);
-    clearGrid(slider.value);
+    sliderValue.textContent = `${slider.value} x ${slider.value} grid`;
+    clearGrid();
     gridConstruction(slider.value);
     drawOnBlock();
+  });
+  clear[0].addEventListener("click", () => {
+    cleanGrid();
   });
   gridConstruction(slider.value);
   drawOnBlock();
 }
 
-function clearGrid(blockWidth) {
-  //   body.remove("gridContainer");
-  //   const grid = body.getElementsByClassName("grid");
-  //   const gridContainer = grid[0].getElementsByClassName("gridContainer");
-  //   grid[0].removeChild(gridContainer[0]);
+function cleanGrid() {
+  const gridBlocks = document.querySelectorAll(".gridBlock");
+  gridBlocks.forEach((gridBlock) => {
+    gridBlock.style.backgroundColor = "white";
+  });
+}
+
+function clearGrid() {
   const gridElements = document.querySelectorAll(".gridContainer");
   gridElements.forEach((element) => element.parentNode.removeChild(element));
 }
@@ -33,12 +35,10 @@ function gridConstruction(blockWidth) {
   gridContainer.classList.add("gridContainer");
   grid[0].appendChild(gridContainer);
 
-  let borderSize = 1 / blockWidth;
-  let dimentions = 650 / blockWidth - borderSize;
+  let dimentions = 650 / blockWidth - 1;
   for (let i = 0; i < blockWidth; i++) {
     const rowGridContainer = document.createElement("div");
     rowGridContainer.classList.add("rowGridContainer");
-    // rowGridContainer.style.borderRight = `solid black 1px`;
     gridContainer.appendChild(rowGridContainer);
     for (let j = 0; j < blockWidth; j++) {
       const newDiv = document.createElement("div");
@@ -50,17 +50,11 @@ function gridConstruction(blockWidth) {
       rowGridContainer.appendChild(newDiv);
     }
   }
-  let gridDimentions = dimentions * blockWidth + 20;
-  console.log(gridDimentions);
+  let gridDimentions = dimentions * blockWidth;
   gridContainer.style.width = gridDimentions;
   gridContainer.style.height = gridDimentions;
   gridContainer.style.borderBottom = "solid black 1px";
   gridContainer.style.borderRight = "solid black 1px";
-
-  const gridWidth = blockWidth * blockWidth + parseInt(blockWidth);
-  console.log(dimentions);
-  //   gridContainer.style.width = `${gridWidth}px`;
-  //   gridContainer.style.height = `${gridWidth}px`;
 }
 
 function drawOnBlock() {
